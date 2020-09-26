9ad5fdb5add2a1c380fb9b22d1e8472867b63aa2
Gaohy <2338944731@qq.com>
2020-09-26 18:27:27 +0800

    将system注册到eureka

---
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -0,0 +1,22 @@
+package com.course.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SystemApplication {
+    private static final Logger Log = LoggerFactory.getLogger(SystemApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(SystemApplication.class);
+        ConfigurableEnvironment environment = springApplication.run(args).getEnvironment();
+        Log.info("启动成功！");
+        Log.info("system 项目地址：\t http://127.0.0.1:{} ",environment.getProperty("server.port"));
+    }
+
+}
