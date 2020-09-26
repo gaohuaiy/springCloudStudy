233b6bf4171a006cb6ff26a2aa906f238a9be382
Gaohy <2338944731@qq.com>
2020-09-26 17:19:57 +0800

    解决eureka启动报错

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,13 +1,26 @@
 package com.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
+    }*/
+    private static final Logger Log = LoggerFactory.getLogger(EurekaApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(EurekaApplication.class);
+        ConfigurableEnvironment environment = springApplication.run(args).getEnvironment();
+        Log.info("启动成功！");
+        Log.info("Eureka 项目地址：\t http://127.0.0.1:{} ",environment.getProperty("server.port"));
     }
 
 }
