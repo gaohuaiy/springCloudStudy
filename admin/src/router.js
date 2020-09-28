fcdee9a280a753bc931ea07ebc2e43c983cca24e
huaiyu.gao <2338944731@qq.com>
2020-09-28 12:09:57 +0800

    新增chapter子路由，并使用假数据加载表格

--- admin/src/router.js
+++ admin/src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router"
 import Login from "./views/login.vue"
 import Admin from "./views/admin.vue"
 import Welcome from "./views/admin/welcome.vue"
+import Chapter from "./views/admin/chapter.vue"
 Vue.use(Router);
 
 export default new Router({
@@ -21,6 +22,10 @@ export default new Router({
             path: "welcome",
             name: "welcome",
             component: Welcome,
+        },{
+            path: "chapter",
+            name: "chapter",
+            component: Chapter,
         }]
     }
 
