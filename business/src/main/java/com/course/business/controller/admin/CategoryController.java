0286d61e44bd383657fb484d65e0ef588fe4dd0b
Gaohy <2338944731@qq.com>
2020-10-07 23:26:18 +0800

    分类列表展示，分为左右两个

--- business/src/main/java/com/course/business/controller/admin/CategoryController.java
+++ business/src/main/java/com/course/business/controller/admin/CategoryController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/category")
@@ -29,6 +30,16 @@ public class CategoryController {
         ResponseDto responseDto = new ResponseDto();
         categoryService.list(pageDto);
         responseDto.setContent(pageDto);
+        return responseDto;
+    }
+    /**
+     * 列表查询
+     */
+    @PostMapping("/all")
+    public ResponseDto all() {
+        ResponseDto responseDto = new ResponseDto();
+        List<CategoryDto> categoryDtoList = categoryService.all();
+        responseDto.setContent(categoryDtoList);
         return responseDto;
     }
 
