c3a887fab5139c7bf850dc191c22a72a13e3e33a
Gaohy <2338944731@qq.com>
2020-10-07 22:22:16 +0800

    新增事务配置

---
+++ server/src/main/java/com/course/server/config/TransactionManagementConfig.java
@@ -0,0 +1,9 @@
+package com.course.server.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configurable
+public class TransactionManagementConfig {
+}
