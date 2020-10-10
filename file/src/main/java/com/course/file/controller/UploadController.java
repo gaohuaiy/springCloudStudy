f465663e51b4124617c6193961e73ad6b86d383d
Gaohy <2338944731@qq.com>
2020-10-11 00:08:14 +0800

    讲师头像优化

--- file/src/main/java/com/course/file/controller/UploadController.java
+++ file/src/main/java/com/course/file/controller/UploadController.java
@@ -4,6 +4,7 @@ import com.course.server.dto.ResponseDto;
 import com.course.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,11 @@ import java.io.IOException;
 public class UploadController {
     private static final Logger LOG = LoggerFactory.getLogger(UploadController.class);
     public static final String BUSINESS_NAME = "文件上传";
+    @Value("${file.path}")
+    private String FILE_PATH;
+    @Value("${file.domain}")
+    private String FILE_DOMAIN;
+
     @PostMapping("/upload")
     public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
         LOG.info("文件上传开始：{}",file);
@@ -27,13 +33,13 @@ public class UploadController {
         //保存文件到本地
         String filename = file.getOriginalFilename();
         String key = UuidUtil.getShortUuid();
-        String fullPath = "D:/file/imooc/teacher/"+key+"-"+filename;
+        String fullPath = FILE_PATH+key+"-"+filename;
         File dest = new File(fullPath);
         file.transferTo(dest);
         LOG.info(dest.getAbsolutePath());
 
         ResponseDto responseDto = new ResponseDto();
-
+        responseDto.setContent(FILE_DOMAIN+"f/teacher/"+key+"-"+filename);
         return responseDto;
     }
 
