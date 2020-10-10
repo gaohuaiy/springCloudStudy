ab35891e4a9c718ac36fc42bb48a3285bb028e12
Gaohy <2338944731@qq.com>
2020-10-10 23:07:10 +0800

    增加file模块

---
+++ file/src/main/java/com/course/file/config/FileApplication.java
@@ -0,0 +1,26 @@
+package com.course.file.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("com.course")
+@MapperScan("com.course.server.mapper")
+public class FileApplication {
+    private static final Logger Log = LoggerFactory.getLogger(FileApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(FileApplication.class);
+        ConfigurableEnvironment environment = springApplication.run(args).getEnvironment();
+        Log.info("启动成功！");
+        Log.info("File 项目地址：\t http://127.0.0.1:{} ",environment.getProperty("server.port"));
+    }
+
+}
