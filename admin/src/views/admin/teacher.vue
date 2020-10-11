1c0f12f19fb98b666373cfe3313dfd64ef652d3c
Gaohy <2338944731@qq.com>
2020-10-11 17:27:08 +0800

    上传文件成功后返回fileDto

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -323,7 +323,7 @@
             },
             afterUpload(resp){
                 let _this = this;
-                let image = resp.content;
+                let image = resp.content.path;
                 _this.teacher.image = image;
             }
         }
